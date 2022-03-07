270104b90fe22378e5b430de5d78c866e037dd34
Sooraj vijayan <96520760+SOORAJVIJAYAN47@users.noreply.github.com>
2022-03-07 21:37:32 +0530

    Create rectangle 3.py

---
+++ rectangle 3.py
@@ -0,0 +1,19 @@
+class Rectangle:
+    def _init_(self,length,width):
+        self.__length=length
+        self.__width=width
+    def area(self):
+        return self._length*self._width
+    def _Lt_(self,other):
+        return self.area()>other.area()
+    L1=int(input("enter the length of first rectangle:"))
+    W1=int(input("enter the width of the first rectangle:"))
+    rectangle1=(L1,W1)
+    L2=int(input("enter the length of the second rectangle:"))
+    W2=int(input("enter the width of the second rectangle:"))
+    rectangle2=(L2,W2)
+    if rectangle1<rectangle2:
+        print("area of rectangle1 is smaller")
+    else:
+        print("area of rectangle2 is smaller")
+   
